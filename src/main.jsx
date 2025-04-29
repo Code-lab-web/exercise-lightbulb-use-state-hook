4330f492a9203a893a688834705a5d6f739a7c98
Johan Skarpsvärd <johanconsult@protonmail.com>
2025-04-29 16:00:42 +0200

    git commit -m "Code-lab-web"

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,10 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import { App } from "./App.jsx";
-import "./styles.css";
+import "./components/lightbulb.jsx";
+import "./components/Lightbulb.jsx";
+import "./components/styles.css";
+import "./components/index.html";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
